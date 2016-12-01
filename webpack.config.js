f9f02681112e182292468ff219aadb747f7200c6
Alexander Gladysh <agladysh@gmail.com>
2016-12-02 00:46:20 +0300

    tmp

--- webpack.config.js
+++ webpack.config.js
@@ -14,12 +14,6 @@ module.exports = {
   },
   module: {
     loaders: [{
-      test: /\.js$/,
-      include: [
-        path.resolve(__dirname, "./src")
-      ],
-      loader: "babel"
-    }, {
       test: /\.css$/,
       loader: 'style!css'
     }]
